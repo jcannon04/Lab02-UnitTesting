14bd7ad7ee2d1027b7d42f483cef1bb7e5280cc9
jcannon04 <123973436+jcannon04@users.noreply.github.com>
2023-07-08 16:14:22 -0500

    add unit testing for atm

--- Lab02-Tests/UnitTest1.cs
+++ Lab02-Tests/UnitTest1.cs
@@ -1,10 +1,69 @@
-﻿namespace Lab02_Tests;
-
-public class UnitTest1
+﻿
+namespace Lab02_Tests
 {
-    [Fact]
-    public void Test1()
+    public class Lab02Test
     {
+        [Fact]
+        public void Withdraw_WithAmountGreaterThanBalance_ReturnsOriginalBalance()
+        {
+            decimal Balance = Lab02_ATM.ATM.Balance;
+            decimal withdrawalAmount = Balance + 1;
+
+            decimal newBalance = Lab02_ATM.ATM.Withdraw(withdrawalAmount);
+
+            Assert.Equal(Balance, newBalance);
+        }
+
+        [Fact]
+        public void Withdraw_WithNegativeAmount_ReturnsOriginalBalance()
+        {
+            decimal Balance = Lab02_ATM.ATM.Balance;
+            decimal withdrawalAmount = -1;
+
+            decimal newBalance = Lab02_ATM.ATM.Withdraw(withdrawalAmount);
+
+            Assert.Equal(Balance, newBalance);
+        }
+        [Fact]
+
+        public void Withdraw_WithValidAmount_ReturnsNewBalance()
+        {
+            decimal Balance = Lab02_ATM.ATM.Balance;
+            decimal withdrawalAmount = Balance - 1;
+
+            decimal newBalance = Lab02_ATM.ATM.Withdraw(withdrawalAmount);
+
+            Assert.Equal(Balance - withdrawalAmount, newBalance);
+        }
+
+        [Fact]
+        public void Deposit_WithNegativeAmount_ReturnsOriginalBalance()
+        {
+            decimal Balance = Lab02_ATM.ATM.Balance;
+            decimal depositAmount = -1;
+            decimal newBalance = Lab02_ATM.ATM.Deposit(depositAmount);
+
+            Assert.Equal(Balance, newBalance);
+        }
+
+        [Fact]
+        public void Deposit_WithValidAmount_ReturnsNewBalance()
+        {
+            decimal Balance = Lab02_ATM.ATM.Balance;
+            decimal depositAmount = 1;
+            decimal newBalance = Lab02_ATM.ATM.Deposit(depositAmount);
+
+            Assert.Equal(Balance + depositAmount, newBalance);
+        }
+
+        [Fact]
+        public void ViewBalance_ReturnsCurrentBalance()
+        {
+            decimal Balance = Lab02_ATM.ATM.Balance;
+            decimal viewedBalance = Lab02_ATM.ATM.ViewBalance();
+
+            Assert.Equal(Balance, viewedBalance);
+        }
 
     }
 }
--- Lab02-UnitTesting/Program.cs
+++ Lab02-UnitTesting/Program.cs
@@ -1,64 +1,86 @@
-﻿class ATM
+﻿namespace Lab02_ATM
 {
-    static public decimal Balance;
 
-    static decimal Withdraw(decimal amount)
+    public class ATM
     {
-        return Balance - amount;
-    }
+        public static decimal Balance = 5;
 
-    static decimal Deposit(decimal amount)
-    {
-        return Balance + amount;
-    }
-
-    static decimal ViewBalance()
-    {
-        return Balance;
-    }
-
-    static void UserInterface()
-    {
-        string userInput = "";
-
-        while (userInput != "exit")
+        public static decimal Withdraw(decimal amount)
         {
+            decimal newBalance = Balance - amount;
 
-            Console.WriteLine("Welcome to the ATM, please select from the following:\n\nwithdraw, deposit, view balance or type exit to quit\n");
-            userInput = Console.ReadLine();
-
-            decimal amount;
-            decimal newBalance = ViewBalance();
-
-            switch (userInput)
+            if (newBalance < 0 || amount < 0)
             {
-                case "withdraw":
-                    Console.WriteLine("How much would you like to withdraw?");
-                    amount = Convert.ToDecimal(Console.ReadLine());
-                    newBalance = Withdraw(amount);
-                    Console.WriteLine($"Withdrawal successful! You withdrew ${amount}, and your new balance is {newBalance}");
-                    break;
-
-                case "deposit":
-                    Console.WriteLine("How much would you like to withdraw?");
-                    amount = Convert.ToDecimal(Console.ReadLine());
-                    newBalance = Deposit(amount);
-                    Console.WriteLine($"Deposit successful! You deposited ${amount}, and your new balance is {newBalance}");
-                    break;
-
-                case "view balance":
-                    Console.WriteLine($"Your current balance is {newBalance}");
-                    break;
-
-                default:
-                    break;
-                      
+                return Balance;
+            }
+            else
+            {
+                Balance -= amount;
+                return Balance;
             }
         }
-    }
 
-    static void Main()
-    {
-        UserInterface();
+        public static decimal Deposit(decimal amount)
+        {
+            if (amount < 0)
+            {
+                return Balance;
+            }
+            else
+            {
+                Balance += amount;
+                return Balance;
+            }
+        }
+
+        public static decimal ViewBalance()
+        {
+            return Balance;
+        }
+
+        static void UserInterface()
+        {
+            string userInput = "";
+
+            while (userInput != "exit")
+            {
+
+                Console.WriteLine("Welcome to the ATM, please select from the following:\n\nwithdraw, deposit, view balance or type exit to quit\n");
+                userInput = Console.ReadLine();
+
+                decimal amount;
+                decimal newBalance = ViewBalance();
+
+                switch (userInput)
+                {
+                    case "withdraw":
+                        Console.WriteLine("How much would you like to withdraw?");
+                        amount = Convert.ToDecimal(Console.ReadLine());
+                        newBalance = Withdraw(amount);
+                        Console.WriteLine($"Withdrawal successful! You withdrew ${amount}, and your new balance is {newBalance}");
+                        break;
+
+                    case "deposit":
+                        Console.WriteLine("How much would you like to withdraw?");
+                        amount = Convert.ToDecimal(Console.ReadLine());
+                        newBalance = Deposit(amount);
+                        Console.WriteLine($"Deposit successful! You deposited ${amount}, and your new balance is {newBalance}");
+                        break;
+
+                    case "view balance":
+                        Console.WriteLine($"Your current balance is {newBalance}");
+                        break;
+
+                    default:
+                        break;
+
+                }
+            }
+        }
+
+        static void Main()
+        {
+            UserInterface();
+        }
     }
 }
